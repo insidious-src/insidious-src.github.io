c4fb3943aba4fa66ebd2168865e2eca5f23d8025
K. Petrov <fymfifa@gmail.com>
2023-01-23 12:48:16 +0200

    electicity.js reference removed

--- js/scripts.contact.js
+++ js/scripts.contact.js
@@ -2,4 +2,4 @@
 layout: js_compress
 sitemap: 'exclude'
 ---
-{% include js/jquery.min.js %}{% include js/jquery.scrollex.min.js %}{% include js/jquery.scrolly.min.js %}{% include js/lazysizes.min.js %}{% include js/skel.min.js %}{% include js/electricity.js %}{% include js/util.min.js %}{% include js/main.min.js %}{% include js/google/recaptcha.min.js %}{% include js/jquery.ajax.contact-recaptcha.min.js %}
+{% include js/jquery.min.js %}{% include js/jquery.scrollex.min.js %}{% include js/jquery.scrolly.min.js %}{% include js/lazysizes.min.js %}{% include js/skel.min.js %}{% include js/util.min.js %}{% include js/main.min.js %}{% include js/google/recaptcha.min.js %}{% include js/jquery.ajax.contact-recaptcha.min.js %}
